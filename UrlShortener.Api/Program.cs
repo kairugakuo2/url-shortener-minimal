9015b46935200696e992a14f03a3e3dca9211849
Gakuo <kairugakuo9@gmail.com>
2025-09-17 21:41:45 -0500

    Scaffolded minimal API w/ Swagger UI and root redirect

---
+++ UrlShortener.Api/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// 1 - SERVICES
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "Url Shortener API", Version = "v1"});
+});
+
+var app = builder.Build();
+
+////////////////////////////////////////////////////////////
+
+// Configure the HTTP request pipeline.
+// 2 - PIPELINE
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Url Shortener API v1");
+        c.RoutePrefix = "swagger"; //UI at /swagger
+    });
+}
+
+// ROOT -> REDIRECT TO SWAGGER
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+// Sample endpoint (default template)
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+// RECORD TYPE
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
